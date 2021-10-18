9901de762082e2305c1ff61e1100f4e3faf2d9d6
katlego phala <kph@bt-industrial.co.za>
2021-10-18 12:24:21 +0200

    added studentapp folder

---
+++ studentapp/views.py
@@ -0,0 +1,36 @@
+
+from django.shortcuts import render, redirect
+from .forms import StudentForm
+from .models import Student
+
+# Create your views here.
+
+
+def student_list(request):
+    context = {'student_list': Student.objects.all()}
+    return render(request, "studentapp/templates/student_list.html", context)
+
+
+def student_form(request, id=0):
+    if request.method == "GET":
+        if id == 0:
+            form = StudentForm()
+        else:
+            student = Student.objects.get(pk=id)
+            form = StudentForm(instance=student)
+        return render(request, "studentapp/templates/student_form.html", {'form': form})
+    else:
+        if id == 0:
+            form = StudentForm(request.POST)
+        else:
+            student = Student.objects.get(pk=id)
+            form = StudentForm(request.POST,instance= student)
+        if form.is_valid():
+            form.save()
+        return redirect('/student/list')
+
+
+def student_delete(request,id):
+    student = Student.objects.get(pk=id)
+    student.delete()
+    return redirect('/student/list')
